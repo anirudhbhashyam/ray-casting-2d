871ba93455fe57216659006b845d4a762369b303
bhashyam.anirudh <bhashyam.anirudh@gmail.com>
2022-12-31 14:56:21 +0100

    Added a world that handles objects and adjusted the game interface.

--- src/game.py
+++ src/game.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, InitVar
 
 import numpy as np
 
@@ -7,12 +7,76 @@ import pygame
 from window import Window
 from particle import Particle
 from boundary import Boundary
-from ray import Ray
+
+
+@dataclass
+class World:
+    n_particles: InitVar[int]
+    n_walls: InitVar[int]
+    world_dimensions: InitVar[tuple[int, int]]
+    particles: list[Particle] = field(
+        default_factory = list
+    )
+    walls: list[Boundary] = field(
+        default_factory = list
+    )
+
+    def __post_init__(self, n_particles, n_walls, world_dimensions) -> None:
+        world_w, world_h = world_dimensions
+        for _ in range(n_particles):
+            self.particles.append(
+                Particle(
+                    pos = np.random.default_rng().uniform(0, world_h, size = 2),
+                    radius = 4.0
+                )
+            )
+        for _ in range(n_walls):
+            self.walls.append(
+                Boundary(
+                    a = np.random.default_rng().uniform(0, world_w, size = 2),
+                    b = np.random.default_rng().uniform(0, world_h, size = 2)
+                )
+            )
+
+        # Add boundaries to the world.
+        self.walls.extend(
+            [
+                Boundary(a = (0, 0), b = (world_w, 0)),
+                Boundary(a = (world_w, 0), b = (world_w, world_h)),
+                Boundary(a = (world_w, world_h), b = (0, world_h)),
+                Boundary(a = (0, world_h), b = (0, 0))
+            ]
+        )
+
+    def draw(self, screen: pygame.Surface) -> None:
+        for wall in self.walls:
+            wall.draw(screen)
+
+        for particle in self.particles:
+            particle.draw(screen)
+
+        for particle in self.particles:
+            for intersection in particle.intersections:
+                pygame.draw.aaline(
+                    screen,
+                    (255, 0, 0),
+                    particle.pos,
+                    intersection
+                )
+
+    def update(self) -> None:
+        for particle in self.particles:
+            particle.update(*pygame.mouse.get_pos())
+
+        for particle in self.particles:
+            particle.look(self.walls)
+
 
 @dataclass
 class Game:
     fps: int
     window: Window
+    world: World
     clock: pygame.time.Clock = field(
         default_factory = pygame.time.Clock
     )
@@ -23,20 +87,6 @@ class Game:
     def run(self) -> None:
         running = True
         background_color = (30, 30, 30)
-
-        p = Particle(
-            pos = np.array([100, 100], dtype = np.float32),
-            radius = 4.0
-        )
-        # ray = Ray(
-        #     pos = np.array([100, 100], dtype = np.float32),
-        #     direction = np.array([4, 3], dtype = np.float32)
-        # )
-
-        wall = Boundary(
-            a = np.array([300, 200], dtype = np.float32),
-            b = np.array([300, 600], dtype = np.float32)
-        )
 
         while running:
             self.clock.tick(self.fps)
@@ -51,28 +101,8 @@ class Game:
 
             self.window.fill_background(background_color)
 
-            wall.draw(self.window.screen)
-            p.draw(self.window.screen)
-
-
-            for intersection in p.look([wall]):
-                pygame.draw.aaline(
-                    self.window.screen,
-                    (255, 0, 0),
-                    p.pos,
-                    intersection
-                )
-
-            p.update(*pygame.mouse.get_pos())
-            # for ray in p.rays:
-            #     if ray.cast(wall):
-            #         ray.color = (255, 0, 0)
-            #     else:
-            #         ray.color = (255, 255, 0)
-            # t = ray.cast(wall)
-            # if t is not None:
-            #     ray.set_color((255, 0, 0))
-
+            self.world.draw(self.window.screen)
+            self.world.update()
             self.window.update()
 
         pygame.quit()
