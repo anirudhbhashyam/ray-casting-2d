78dee9d4f8af447820947c513d28312242a997c5
bhashyam.anirudh <bhashyam.anirudh@gmail.com>
2022-12-31 14:55:06 +0100

    Adjusted main to incorporate the world.

--- main.py
+++ main.py
@@ -15,18 +15,26 @@ sys.path.append(
 import config
 from window import Window
 from game import Game
+from game import World
 
 def main() -> int:
     config_settings = config.load(os.path.join(CPD, "config", "settings.json"))
     game_settings = config_settings["GAME_SETTINGS"]
+    world_settings = config_settings["WORLD_SETTINGS"]
     win = Window(
         width = game_settings["WIDTH"],
         height = game_settings["HEIGHT"],
         caption = game_settings["TITLE"]
     )
+    w = World(
+        n_particles = world_settings["N_PARTICLES"],
+        n_walls = world_settings["N_WALLS"],
+        world_dimensions = (world_settings["WIDTH"], world_settings["HEIGHT"])
+    )
     g = Game(
         fps = game_settings["FPS"],
-        window = win
+        window = win,
+        world = w
     )
     g.run()
     return 0
